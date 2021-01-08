1a9c9ed283d5ce727b677ba04d8f184c0ff45d47
m27bay <mickael.ledenmat@gmail.com>
2021-01-08 11:34:22 +0100

    add menu (not finish yet)

---
+++ src/lib/Menu.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "Menu.hpp"
+
+Menu::Menu() {
+  // Empty
+}
+
+void Menu::run() {
+  std::cout << "hello wordl" << std::endl;
+}
+
+Menu::~Menu() {
+  // Empty
+}
---
+++ src/lib/Menu.hpp
@@ -0,0 +1,20 @@
+#ifndef MENU_HPP
+#define MENU_HPP
+
+class Menu
+{
+private:
+  /* data */
+
+public:
+  /* Constructor */
+  Menu(/* args */);
+
+  /* Functions */
+  void run();
+
+  /* Destructor */
+  ~Menu();
+};
+
+#endif
